bb79b5f567cd91e813cbbc9d10d1f4b024c331de
Thierry Boiago <thierry123123@gmail.com>
2024-10-23 17:59:13 -0300

    finalização

--- src/App.js
+++ src/App.js
@@ -1,102 +1,138 @@
+import Input from "./components/Input";
+import Button from "./components/Button";
 
-import Input from './components/Input';
-import Button from './components/Button';
-
-import { Container, Content, Row } from './styles';
-import { useState } from 'react';
-
+import { Container, Content, Row } from "./styles";
+import { useState, useEffect } from "react";
 
 const App = () => {
-  const [currentNumber, setCurrentNumber] = useState('0');
-  const [firstNumber, setFirstNumber] = useState('0');
-  const [operation, setOperation] = useState('');
+  const [currentNumber, setCurrentNumber] = useState("0");
+  const [currentLogic, setcurrentLogic] = useState("");
+  const [firstNumber, setFirstNumber] = useState("0");
+
+  const [secondNumber, setSecondNumber] = useState("0");
+
+  const [currentOperation, setOperation] = useState("");
 
   const handleOnClear = () => {
-    setCurrentNumber('0')
-    setFirstNumber('0')
-    setOperation('')
+    setCurrentNumber("0");
+    setFirstNumber("0");
+    setSecondNumber("0");
+    setOperation("");
+    setcurrentLogic("");
   };
 
+  useEffect(() => {
+    calculate();
+    if (firstNumber !== "0" && secondNumber === "0") {
+      setcurrentLogic(firstNumber + " " + currentOperation);
+      setCurrentNumber("0");
+    }
+  }, [firstNumber, secondNumber]);
+
   const handleAddNumber = (num) => {
-    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
-  }
-
-  const handleSumNumbers = () => {
-
-    if(firstNumber === '0'){
-        setFirstNumber(String(currentNumber));
-        setCurrentNumber('0')
-        setOperation('+')
-    }else {
-      const sum = Number(firstNumber) + Number(currentNumber);
-      setCurrentNumber(String(sum))
-      setOperation('')
+    if(firstNumber !== "0" && secondNumber !== "0" && currentOperation !== ""){
+      handleOnClear();
     }
+    setCurrentNumber((prev) => `${prev === "0" || prev === "00" ? "" : prev}${num}`);
+  };
 
-  }
-
-  const handleMinusNumbers = () => {
-
-    if(firstNumber === '0'){
+  const handleOperation = (operation) => {
+    if (currentOperation === "") {
+      setOperation(operation);
+      if (firstNumber === "0") {
         setFirstNumber(String(currentNumber));
-        setCurrentNumber('0')
-        setOperation('-')
-    }else {
-      const sum = Number(firstNumber) - Number(currentNumber);
-      setCurrentNumber(String(sum))
-      setOperation('')
+      } else if (firstNumber === "0") {
+        setSecondNumber(String(currentNumber));
+      } else {
+        setFirstNumber(String(currentNumber));
+        setSecondNumber("0");
+      }
+    } else {
+      if (firstNumber !== "0" && secondNumber !== "0") {
+        setOperation(operation);
+        setFirstNumber(String(currentNumber));
+        setSecondNumber("0");
+      }
     }
+  };
 
-  }
+  const calculate = () => {
+    if (
+      firstNumber !== "0" &&
+      secondNumber !== "0" &&
+      currentOperation !== ""
+    ) {
+      switch (currentOperation) {
+        case "+":
+          setCurrentNumber(String(Number(firstNumber) + Number(secondNumber)));
+          break;
+        case "-":
+          setCurrentNumber(String(Number(firstNumber) - Number(secondNumber)));
+
+          break;
+        case "*":
+          setCurrentNumber(String(Number(firstNumber) * Number(secondNumber)));
+          break;
+        case "/":
+          setCurrentNumber(String(Number(firstNumber) / Number(secondNumber)));
+          break;
+        default:
+          break;
+      }
+
+      setcurrentLogic(
+        firstNumber + " " + currentOperation + " " + secondNumber + " = "
+      );
+    }
+  };
 
   const handleEquals = () => {
-
-    if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
-        switch(operation){
-          case '+':
-            handleSumNumbers();
-            break;
-          case '-':
-            handleMinusNumbers();
-            break;
-          default: 
-            break;
-        }
+    if (secondNumber === "0") {
+      setSecondNumber(String(currentNumber));
+    } else {
+      setFirstNumber(String(currentNumber));
     }
-
-  }
+  };
 
   return (
     <Container>
       <Content>
-        <Input value={currentNumber}/>
+        <Input value={currentLogic} />
+        <Input value={currentNumber} />
         <Row>
-          <Button label="x"/>
-          <Button label="/"/>
-          <Button label="c" onClick={handleOnClear}/>
-          <Button label="."/>
+          <Button label="x" onClick={() => handleOperation("*")} />
+          <Button label="/" onClick={() => handleOperation("/")} />
+          <Button label="c" onClick={handleOnClear} />
+          <Button label="." />
         </Row>
         <Row>
-          <Button label="7" onClick={() => handleAddNumber('7')}/>
-          <Button label="8" onClick={() => handleAddNumber('8')}/>
-          <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={handleMinusNumbers}/>
+          <Button label="7" onClick={() => handleAddNumber("7")} />
+          <Button label="8" onClick={() => handleAddNumber("8")} />
+          <Button label="9" onClick={() => handleAddNumber("9")} />
+          <Button label="-" onClick={() => handleOperation("-")} />
         </Row>
         <Row>
-          <Button label="4" onClick={() => handleAddNumber('4')}/>
-          <Button label="5" onClick={() => handleAddNumber('5')}/>
-          <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={handleSumNumbers}/>
+          <Button label="4" onClick={() => handleAddNumber("4")} />
+          <Button label="5" onClick={() => handleAddNumber("5")} />
+          <Button label="6" onClick={() => handleAddNumber("6")} />
+          <Button label="+" onClick={() => handleOperation("+")} />
         </Row>
         <Row>
-          <Button label="1" onClick={() => handleAddNumber('1')}/>
-          <Button label="2" onClick={() => handleAddNumber('2')}/>
-          <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="=" onClick={handleEquals}/>
+          <Button label="1" onClick={() => handleAddNumber("1")} />
+          <Button label="2" onClick={() => handleAddNumber("2")} />
+          <Button label="3" onClick={() => handleAddNumber("3")} />
+          <Button label="=" onClick={handleEquals} />
+        </Row>
+
+        <Row>
+          <Button label="00" onClick={() => handleAddNumber("00")} />
+          <Button label="0" onClick={() => handleAddNumber("0")} />
+          <Button label="." onClick={() => handleAddNumber(".")} />
+          <Button label="=" onClick={handleEquals} />
         </Row>
       </Content>
     </Container>
   );
-}
+};
 
 export default App;
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const InputContainer = styled.div`
     width: 100%;
     height: 75px;
-    background-color: #AAAAFF;
+    background-color: #242424;
 
     display: flex;
     align-items: center;
@@ -16,7 +16,7 @@ export const InputContainer = styled.div`
     input {
         width: 100%;
         height: 75px;
-        background-color: #AAAAFF;
+        background-color: #242424;
         border: 0;
         display: flex;
         flex-direction: column;
@@ -24,6 +24,7 @@ export const InputContainer = styled.div`
         padding: 0 10px;
         font-size: 24px;
         font-family: 'Roboto';
+        text-align:right; 
         color: #FFFFFF;
     }
 `
